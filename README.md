374a1ed359d02e0c27e2cef5d53f067fd794ea7d
Jon Cowie <jcowie@etsy.com>
2013-06-26 09:45:35 +0100

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,36 @@ jawboneup_to_graphite
 =====================
 
 A little script for sending Sleep metrics from your Jawbone Up to Graphite
+
+##Installation
+
+This script requires a couple of gems to function. Run the following ```gem install``` commands before using:
+
+```
+gem install choice
+gem install jawbone-up
+```
+
+##Instructions
+
+Before using the script, you'll need to get a XID and Token from Jawbone to use with the script. To do this, run the script with the ```--get-token``` option and enter your Jawbone Up username and password as below. The script will then return an XID and Token.
+
+```
+$> ./jawboneup_to_graphite.rb --get-token
+Jawbone Username: me@mydomain.com
+Jawbone Password: mypassword
+Token: bhdoijfoijdoipjfdspad
+Xid: sdsajdosajdioaj
+```
+
+The next step is to copy your XID and Token into the ```xid``` and ```token``` variables at the top of the ```jawboneup_to_graphite``` script. You'll also want to set your graphite host, port, and the prefix you want to give your metrics:
+
+```
+xid = 'bhdoijfoijdoipjfdspad'
+token = 'sdsajdosajdioaj'
+graphite_host = "my.graphite.server"
+graphite_port = "2003"
+metric_prefix = "me.jawbone.sleep"
+```
+
+Now you just need to run the script whenever you Sync your Jawbone Up, and it'll send your sleep metrics to graphite ready for adding to Dashboard, creating Nagios alerts and so on.
---
+++ jawboneup_to_graphite.rb
@@ -0,0 +1,83 @@
+#!/usr/bin/env ruby
+
+require 'jawbone-up'
+require 'date'
+require 'choice'
+
+# PARAMETERS
+
+# Run this script with the --get-token option first
+# Then copy the xid and token returned into the below parameters
+# You can also customise the graphite host, port and metric prefix.
+
+xid = ''
+token = ''
+graphite_host = "my.graphite.server"
+graphite_port = "2003"
+metric_prefix = "me.jawbone.sleep"
+
+Choice.options do
+  header ""
+  header "Specific options:"
+
+  option :get_token, :required => false do
+    long  '--get-token'
+    desc  'Get a login token from Jawbone'
+  end
+end
+
+if Choice.choices[:get_token]
+  print "Jawbone Username: "
+  username = STDIN.gets.chomp
+  print "Jawbone Password: "
+  password = STDIN.gets.chomp
+  up = JawboneUP::Session.new
+  up.signin username, password
+  puts "Token: #{up.token}"
+  puts "Xid: #{up.xid}"
+else
+    if xid.empty? or token.empty?
+      puts "Invalid token and xid. Please run the script with the --get-token option first, then paste the values into the parameters at the top of the script."
+      exit 1
+    end
+    up = JawboneUP::Session.new :auth => {
+      :xid => xid,
+      :token => token
+    }
+
+    today = Date.today.prev_day.to_time.to_i
+
+    sleep_info = up.get_sleep_summary
+    sleep_info['items'].each do |item|
+    if today < item['time_created']
+      date = Time.at item['time_created']
+      puts "\n"
+      puts date.to_s + " " + item['title']
+      puts "Timestamp: #{item['time_created']}"
+      puts "Light Sleep: #{item['details']['light']/60}"
+      puts "Deep Sleep: #{item['details']['deep']/60}"
+      puts "Woke Up: #{item['details']['awakenings']} time(s)"
+      puts "Sleep Quality: #{item['details']['quality']}"
+
+      puts "Sending #{metric_prefix}.light_sleep to #{graphite_host}"
+      message = "#{metric_prefix}.light_sleep #{item['details']['light']/60} #{item['time_created']}\n"
+      socket = TCPSocket.open(graphite_host, graphite_port)
+      socket.write(message)
+
+      puts "Sending #{metric_prefix}.deep_sleep to #{graphite_host}"
+      message = "#{metric_prefix}.deep_sleep #{item['details']['deep']/60} #{item['time_created']}\n"
+      socket = TCPSocket.open(graphite_host, graphite_port)
+      socket.write(message)
+
+      puts "Sending #{metric_prefix}.awakenings to #{graphite_host}"
+      message = "#{metric_prefix}.awakenings #{item['details']['awakenings']} #{item['time_created']}\n"
+      socket = TCPSocket.open(graphite_host, graphite_port)
+      socket.write(message)
+
+      puts "Sending #{metric_prefix}.quality to #{graphite_host}"
+      message = "#{metric_prefix}.quality #{item['details']['quality']} #{item['time_created']}\n"
+      socket = TCPSocket.open(graphite_host, graphite_port)
+      socket.write(message)
+    end
+  end
+end
